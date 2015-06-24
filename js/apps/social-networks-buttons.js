80bc17bfd98833e72747bfaa6488a2080edf8dec
V@D91 <m.4.r@yandex.ua>
2015-06-25 01:12:51 +0300

    Added facebook authorization handler

--- js/apps/social-networks-buttons.js
+++ js/apps/social-networks-buttons.js
@@ -12,3 +12,18 @@ function onSignIn(googleUser) {
     js.src = "//connect.facebook.net/uk_UA/sdk.js#xfbml=1&version=v2.3&appId=397110820480643";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
+function fbLogin(result){
+  if (typeof result.status !== 'undefined' && result.status === 'connected'){ 
+    FB.login(function(response) {
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+          // Use this data for login and sign up
+          console.log(response);
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    });
+  }
+}
