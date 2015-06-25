941eda00d98e096106b068686b2729200945f62b
acid30m <acid30m@users.noreply.github.com>
2015-06-25 10:34:48 +0300

    Update addValid.js

--- js/apps/addValid.js
+++ js/apps/addValid.js
@@ -4,7 +4,7 @@ function addIvent(){
         $("#email")[0].addEventListener("input", realTimeEmailValid);
         $("#password")[0].addEventListener("input", realTimePassValid);
     }
-    else if($("#sign_up_form")[0]){
+    if($("#sign_up_form")[0]){
         $("#signup_btn")[0].addEventListener("click",function(event){ signUpValidation(event)});
         $("#email")[0].addEventListener("input", realTimeEmailValid);
         $("#password")[0].addEventListener("input", realTimePassValid);
