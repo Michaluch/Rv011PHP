663a5b5651949bcfe911569a24270ad913ab7c5b
acid30m <acid30m@users.noreply.github.com>
2015-06-25 10:43:01 +0300

    Changed adding function

--- js/apps/addValid.js
+++ js/apps/addValid.js
@@ -1,14 +1,14 @@
 function addIvent(){
     if($("#login_form")[0]){
-        $("#login_btn")[0].addEventListener("click",function(event){ validation(event)});
-        $("#email")[0].addEventListener("input", realTimeEmailValid);
-        $("#password")[0].addEventListener("input", realTimePassValid);
+        $("#login_form")[0].find('#login_btn')[0].addEventListener("click",function(event){ validation(event)});
+        $("#login_form")[0].find("#email")[0].addEventListener("input", realTimeEmailValid);
+        $("#login_form")[0].find("#password")[0].addEventListener("input", realTimePassValid);
     }
     if($("#sign_up_form")[0]){
-        $("#signup_btn")[0].addEventListener("click",function(event){ signUpValidation(event)});
-        $("#email")[0].addEventListener("input", realTimeEmailValid);
-        $("#password")[0].addEventListener("input", realTimePassValid);
-        $("#confpass")[0].addEventListener("input", checkConfirm);
+        $("#sign_up_form")[0].find("#signup_btn")[0].addEventListener("click",function(event){ signUpValidation(event)});
+        $("#sign_up_form")[0].find("#email")[0].addEventListener("input", realTimeEmailValid);
+        $("#sign_up_form")[0].find("#password")[0].addEventListener("input", realTimePassValid);
+        $("#sign_up_form")[0].find("#confpass")[0].addEventListener("input", checkConfirm);
     }
 }
 
