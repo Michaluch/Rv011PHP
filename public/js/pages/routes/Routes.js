82481a92b93d55f94409e26b3356fdbf7ff7fad6
Kozulova <benedukvalia@gmail.com>
2015-07-04 23:27:21 +0300

    added View for Login page, without styles and scripts, just Backbone View

--- public/js/pages/routes/Routes.js
+++ public/js/pages/routes/Routes.js
@@ -1,12 +1,12 @@
 define([
 	//libs
 	'backbone',
-	'SignInView'
-
+	'SignInView',
+    "pages/views/LoginView"
 
     ],
 
-    function(Backbone, SignInView) {
+    function(Backbone, SignInView, LoginView) {
         return Backbone.Router.extend({
 
 
@@ -18,8 +18,10 @@ define([
 
 
             index: function() {
-              
-              
+            if(typeof loginView==="undefined"){
+                loginView=new LoginView();
+                };
+                loginView.render(); 
             },
 
             register: function() {
